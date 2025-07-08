89e85b83f2ddf7945680896a9ae3ee6f907ed344
sah024 <sarah.tumelero.da.silveira@gmail.com>
2025-07-08 09:02:05 -0300

    seeds

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
+    use HasFactory;
     protected $fllable = ['title', 'content', 'category_id', 'user_id'];
 
     public function user():BelongsTo {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,7 +14,13 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
+    {   
+        DB::table('users')->insert([
+            'name'=>'admin',
+            'email'=>'adm@adm.com',
+            'password'=>Hash::make('12345678'),
+            'email_verified_at'=>now(),
+        ]);
         User::factory(10)->create();
     }
 }
